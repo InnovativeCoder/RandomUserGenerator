b84b77570159e3f579e8675382c06ec41ce8902d
InnovativeCoder <aashitsingh2010@gmail.com>
2020-05-04 19:10:06 +0530

    Pushed the basic result demo api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class FecthDummyAPI extends Component {
+  state = {
+    loading: true,
+    person: null,
+  };
+
+  async componentDidMount() {
+    const url = "https://api.randomuser.me/?results=5";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({
+      person: data.results[0],
+      loading: false,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.loading || !this.state.person ? (
+          <div> ..loading.. </div>
+        ) : (
+          <div>
+            <div> {this.state.person.name.title} </div>
+            <div> {this.state.person.name.first} </div>
+            <div> {this.state.person.name.last} </div>
+            <img src={this.state.person.picture.large} alt="Pic" />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
